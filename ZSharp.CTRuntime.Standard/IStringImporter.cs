839c4d9fd779f4b67f9e34dd422dd038a6256c50
Binyamin Y Cohen <binyamincohen555@gmail.com>
2024-06-24 18:40:15 +0300

    Add ZSharp.CTRuntime.Standard project

---
+++ ZSharp.CTRuntime.Standard/IStringImporter.cs
@@ -0,0 +1,9 @@
+﻿using ZSharp.VM;
+
+namespace ZSharp.IR.Standard
+{
+    public interface IStringImporter
+    {
+        public ZSObject Import(string source);
+    }
+}
---
+++ ZSharp.CTRuntime.Standard/StringImporter.cs
@@ -0,0 +1,40 @@
+﻿using CommonZ.Utils;
+using ZSharp.VM;
+
+namespace ZSharp.IR.Standard
+{
+    public sealed class StringImporter : IStringImporter
+    {
+        private readonly Mapping<string, IStringImporter> importers = new();
+
+        public IStringImporter? DefaultImporter { get; set; }
+
+        public string Separator { get; set; } = ":";
+
+        public void AddImporter(string type, IStringImporter importer)
+        {
+            importers.Add(type, importer);
+        }
+
+        public IStringImporter GetImporter(string type)
+        {
+            return importers[type];
+        }
+
+        public ZSObject Import(string source)
+        {
+            string[] parts = source.Split(Separator, 2);
+
+            if (parts.Length == 1)
+            {
+                if (DefaultImporter is null) throw new Exception();
+                return DefaultImporter.Import(source);
+            }
+
+            string type = parts[0];
+            source = parts[1];
+
+            return GetImporter(type).Import(source);
+        }
+    }
+}
