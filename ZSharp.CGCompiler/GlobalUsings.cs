16fd4669d0ede15ee544f9cd780f0f7572c38816
Binyamin Y Cohen <binyamincohen555@gmail.com>
2024-08-21 02:00:00 +0300

    Remove unused usings

--- ZSharp.CGCompiler/GlobalUsings.cs
+++ ZSharp.CGCompiler/GlobalUsings.cs
@@ -1,4 +1,2 @@
-﻿global using HLVM = ZSharp.CGRuntime.HLVM;
-
 ﻿global using CGObject = ZSharp.CGRuntime.CGObject;
 global using CGCode = CommonZ.Utils.Collection<ZSharp.CGRuntime.HLVM.Instruction>;
