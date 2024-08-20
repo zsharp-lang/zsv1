45178a0a04e42fb5cf777d8cddb9b0e1ea0d1491
Binyamin Y Cohen <binyamincohen555@gmail.com>
2024-08-21 01:46:13 +0300

    Move binding CG function to CG.Context

--- ZSharp.CGRuntime/cg/CG.Context.cs
+++ ZSharp.CGRuntime/cg/CG.Context.cs
@@ -2,6 +2,14 @@
 {
     public static partial class CG
     {
+        public static HLVM.Binding Get(string name)
+            => HLVM.Binding.Get(name);
+
+        public static HLVM.Binding Set(string name)
+            => HLVM.Binding.Set(name);
+
+        public static HLVM.Binding Del(string name)
+            => HLVM.Binding.Del(name);
 
         public static HLVM.Enter Enter()
             => new();
