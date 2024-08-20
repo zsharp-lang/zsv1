7e79ce0b7b993f30bd32a039d8b178f170580c2d
Binyamin Y Cohen <binyamincohen555@gmail.com>
2024-08-21 01:53:34 +0300

    Add module IR field

--- ZSharp.CGRuntime.Objects/modular/Module.cs
+++ ZSharp.CGRuntime.Objects/modular/Module.cs
@@ -3,6 +3,8 @@
     public sealed class Module(string? name)
         : CGObject
     {
+        public IR.Module? IR { get; set; }
+
         public string? Name { get; set; } = name;
 
         public CGCode Content { get; } = [];
