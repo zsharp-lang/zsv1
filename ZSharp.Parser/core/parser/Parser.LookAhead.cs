75268907616ee78305c8b932812605416a9ac443
Binyamin Y Cohen <binyamincohen555@gmail.com>
2024-08-21 23:15:38 +0300

    Add look ahead context

---
+++ ZSharp.Parser/core/parser/Parser.LookAhead.cs
@@ -0,0 +1,8 @@
+﻿namespace ZSharp.Parser
+{
+    public sealed partial class Parser
+    {
+        public TokenStream.TokenStreamPosition LookAhead() 
+            => TokenStream.LookAhead();
+    }
+}
