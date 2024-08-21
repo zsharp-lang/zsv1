5e4a70f1df2a472d586a43d3b71b769aba537a0b
Binyamin Y Cohen <binyamincohen555@gmail.com>
2024-08-21 23:14:57 +0300

    Rename context parser in main parser

---
+++ ZSharp.Parser/core/parser/Parser.Typed.cs
@@ -0,0 +1,19 @@
+﻿using CommonZ.Utils;
+using ZSharp.AST;
+
+namespace ZSharp.Parser
+{
+    public sealed partial class Parser
+    {
+        private readonly Mapping<Type, ParserBase> parsersFor = [];
+
+        public void AddParserFor<T>(Parser<T> contextParser)
+            where T : Node
+        {
+            parsersFor[typeof(T)] = contextParser;
+        }
+
+        public T Parse<T>() where T : Node
+            => (parsersFor[typeof(T)] as Parser<T>)!.Parse(this);
+    }
+}
