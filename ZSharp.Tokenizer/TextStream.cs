33e8c5abea159ddaf2b8dba62d5366a636db0f6e
Binyamin Y Cohen <binyamincohen555@gmail.com>
2024-08-21 22:34:13 +0300

    Add tokenizer

---
+++ ZSharp.Tokenizer/TextStream.cs
@@ -0,0 +1,56 @@
+﻿using ZSharp.Text;
+
+namespace ZSharp.Tokenizer
+{
+    public sealed class TextStream(TextReader stream)
+    {
+        private readonly TextReader stream = stream;
+
+        public int Index { get; private set; }
+
+        public Position Position { get; private set; } = new(1, 1);
+
+        public bool IsEOF { get; private set; }
+
+        public TextStream(string content)
+            : this(new StringReader(content)) { }
+
+        public string ReadLine()
+        {
+            string line = stream.ReadLine()!;
+            Position = new(Position.Line + 1, 1);
+            return line;
+        }
+
+        public char Peek()
+            => Peek(false);
+
+        public char Read()
+        {
+            char c = Peek(true);
+
+            Position = c switch
+            {
+                '\n' => new(Position.Line + 1, 1),
+                '\r' => Position,
+
+                _ => new(Position.Line, Position.Column + 1)
+            };
+
+            return c;
+        }
+
+        private char Peek(bool read)
+        {
+            int result = read ? stream.Read() : stream.Peek();
+
+            if (result == -1)
+            {
+                IsEOF = true;
+                return '\0';
+            }
+
+            return (char)result;
+        }
+    }
+}
---
+++ ZSharp.Tokenizer/Tokenizer.cs
@@ -0,0 +1,202 @@
+﻿using System.Runtime.CompilerServices;
+using System.Text;
+using ZSharp.Text;
+
+namespace ZSharp.Tokenizer
+{
+    public static class Tokenizer
+    {
+        private const char StringQuotation = '\"';
+        private const char CharQuotation = '\'';
+
+        private const char BackSlash = '\\';
+        private const char Slash = '/';
+
+        private const char Asterisk = '*';
+
+        private const char Underscore = '_';
+
+        private const char LF = '\n';
+        private const char CR  = '\r';
+
+        private const string Operators = "./|+-=<>!@#$%^&*~?";
+
+        private static bool IsAlpha(char c)
+            => ('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z');
+
+        private static bool IsDigit(char c)
+            => '0' <= c && c <= '9';
+
+        private static bool IsHexDigit(char c)
+            => IsDigit(c) || ('a' <= c && c <= 'f') || ('a' <= c && c <= 'f');
+
+        private static bool IsAlphaNumeric(char c)
+            => IsAlpha(c) || IsDigit(c);
+
+        public static IEnumerable<Token> Tokenize(TextStream stream)
+        {
+            while (!stream.IsEOF)
+            {
+                Position start = stream.Position;
+
+                Token SingleChar(TokenType type)
+                    => new(type, stream.Read().ToString(), new(start, stream.Position));
+
+                Token ReadString()
+                {
+                    char c = stream.Read();
+
+                    StringBuilder sb = new();
+
+                    while ((c = stream.Read()) != StringQuotation)
+                    {
+                        if (c == BackSlash)
+                        {
+                            var replace = stream.Peek() switch
+                            {
+                                'n' => LF,
+                                'r' => CR,
+                                StringQuotation => StringQuotation,
+                                CharQuotation => CharQuotation,
+                                BackSlash => stream.Read(),
+                                _ => c
+                            };
+                            if (c != replace)
+                                stream.Read();
+                            c = replace;
+                        }
+
+                        sb.Append(c);
+                    }
+
+                    return new(TokenType.String, sb.ToString(), new(start, stream.Position));
+                }
+
+                Token ReadChar()
+                {
+                    char c = stream.Read(); // assert == CharQuotation
+
+                    if ((c = stream.Read()) == BackSlash)
+                    {
+                        var replace = stream.Peek() switch
+                        {
+                            'n' => LF,
+                            'r' => CR,
+                            StringQuotation => StringQuotation,
+                            CharQuotation => CharQuotation,
+                            BackSlash => stream.Read(),
+                            _ => c
+                        };
+                        if (c != replace)
+                            stream.Read();
+                        c = replace;
+                    }
+
+                    stream.Read(); // assert == CharQuotation
+
+                    return new(TokenType.Char, c.ToString(), new(start, stream.Position));
+                }
+
+                Token ReadNumber()
+                {
+                    throw new NotImplementedException();
+                }
+
+                Token ReadIdentifier()
+                {
+                    char c = stream.Peek();
+
+                    StringBuilder sb = new();
+
+                    if (!IsAlpha(c) && c != Underscore) throw new NotImplementedException();
+                    sb.Append(stream.Read());
+
+                    while (IsAlphaNumeric(c = stream.Peek()) || c == Underscore)
+                        sb.Append(stream.Read());
+
+                    return new(TokenType.Identifier, sb.ToString(), new(start, stream.Position));
+                }
+
+                Token ReadToken()
+                {
+                    char c = stream.Peek();
+
+                    StringBuilder sb = new();
+
+                    if (c == Slash)
+                    {
+                        stream.Read();
+
+                        if (stream.Peek() == Slash)
+                            return new(TokenType.LineComment, stream.ReadLine(), new(start, stream.Position));
+                        if (stream.Peek() == Asterisk)
+                        {
+                            stream.Read();
+
+                            while (!stream.IsEOF)
+                            {
+                                while (stream.Peek() != Asterisk) stream.Read();
+                                stream.Read();
+                                if (stream.Peek() == Slash) break;
+                            }
+
+                            stream.Read();
+                            return new(TokenType.BlockComment, string.Empty, new(start, stream.Position));
+                        }
+
+                        sb.Append(c);
+
+                        while (Operators.Contains(stream.Peek()))
+                            sb.Append(stream.Read());
+
+                        return new(TokenType.Operator, sb.ToString(), new(start, stream.Position));
+                    }
+
+                    if (Operators.Contains(c))
+                    {
+                        while (Operators.Contains(c = stream.Peek()))
+                            sb.Append(stream.Read());
+
+                        return new(TokenType.Operator, sb.ToString(), new(start, stream.Position));
+                    }
+
+                    if (IsDigit(c)) return ReadNumber();
+
+                    if (IsAlpha(c) || c == Underscore) return ReadIdentifier();
+
+                    throw new Exception("Invalid character: " + c);
+                }
+
+                Token EOF()
+                    => new(TokenType.EOF, string.Empty, new(start, stream.Position));
+
+                char c = stream.Peek();
+
+                Token token = c switch
+                {
+                    '\0' => EOF(),
+                    '$' => SingleChar(TokenType.Breakpoint),
+                    LF => SingleChar(TokenType.NewLine),
+                    CR => SingleChar(TokenType.NewLine),
+                    ' ' => SingleChar(TokenType.Space),
+                    '\t' => SingleChar(TokenType.Tab),
+                    '{' => SingleChar(TokenType.LCurly),
+                    '}' => SingleChar(TokenType.RCurly),
+                    '(' => SingleChar(TokenType.LParen),
+                    ')' => SingleChar(TokenType.RParen),
+                    '[' => SingleChar(TokenType.LBracket),
+                    ']' => SingleChar(TokenType.RBracket),
+                    ';' => SingleChar(TokenType.Semicolon),
+                    ':' => SingleChar(TokenType.Colon),
+                    ',' => SingleChar(TokenType.Comma),
+                    StringQuotation => ReadString(),
+                    CharQuotation => ReadChar(),
+                    '0' =>  ReadNumber(),
+                    _ => ReadToken()
+                };
+
+                yield return token;
+            }
+        }
+    }
+}
