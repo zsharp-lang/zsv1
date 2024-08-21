62dc1a7fa27b5768cb7f43fb3249929244989a0c
Binyamin Y Cohen <binyamincohen555@gmail.com>
2024-08-21 23:16:02 +0300

    Update parser main test file

--- ZSharpParserTest/Program.cs
+++ ZSharpParserTest/Program.cs
@@ -8,13 +8,25 @@ using static MyParsers;
 using (StreamReader stream = File.OpenText("./parserText.txt"))
 {
     var parser = new Parser(Tokenizer.Tokenize(new(stream)));
-    parser.AddContextParser(new ExpressionParser());
+    parser.AddParserFor(new ExpressionParser());
 
     var documentParser = new DocumentParser();
 
     documentParser.AddKeywordParser("let", ExpressionStatement(ParseLetExpression));
+    
+    using (var lookAhead = parser.LookAhead())
+    {
+        parser.Eat("let");
 
+        using (var lookAhead2 = parser.LookAhead())
+        {
+            parser.Eat(ZSharp.Text.TokenType.Identifier);
+        }
+
+        lookAhead.Restore();
+    }
     var documentNode = documentParser.Parse(parser);
+
 
     Console.WriteLine($"Finished parsing document with {documentNode.Statements.Count} statements!");
 }
