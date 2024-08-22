316b6e5555282b070d0071430dc1847bc5dad341
Binyamin Y Cohen <binyamincohen555@gmail.com>
2024-08-22 13:26:44 +0300

    Add access to typed parsers

--- ZSharp.Parser/core/parser/Parser.Typed.cs
+++ ZSharp.Parser/core/parser/Parser.Typed.cs
@@ -1,4 +1,5 @@
 ﻿using CommonZ.Utils;
+using System.Diagnostics.CodeAnalysis;
 using ZSharp.AST;
 
 namespace ZSharp.Parser
@@ -12,6 +13,14 @@ namespace ZSharp.Parser
         {
             parsersFor[typeof(T)] = contextParser;
         }
+
+        public Parser<T>? GetParserFor<T>()
+            where T : Node
+            => parsersFor.TryGetValue(typeof(T), out var parser) ? parser as Parser<T> : null;
+
+        public bool GetParserFor<T>([NotNullWhen(true)] out Parser<T>? parser)
+            where T : Node
+            => (parser = GetParserFor<T>()) is not null;
 
         public T Parse<T>() where T : Node
             => (parsersFor[typeof(T)] as Parser<T>)!.Parse(this);
