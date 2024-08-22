1254e635daf094bda7c805a5bcaf91734f822bf8
Binyamin Y Cohen <binyamincohen555@gmail.com>
2024-08-22 15:29:50 +0300

    Add AST resolver

---
+++ ZSharp.Resolver/resolver/Resolver.Expression.cs
@@ -0,0 +1,15 @@
+﻿namespace ZSharp.Resolver
+{
+    public static partial class Resolver
+    {
+        public static RExpression Resolve(Expression expression)
+            => expression switch
+            {
+                LiteralExpression literal => Resolve(literal),
+                _ => NotImplemented<RExpression>(expression)
+            };
+
+        public static RLiteral Resolve(LiteralExpression literalExpression)
+            => Utils.ParseLiteral(literalExpression);
+    }
+}
---
+++ ZSharp.Resolver/resolver/stmt/Resolver.Statement.cs
@@ -0,0 +1,16 @@
+﻿namespace ZSharp.Resolver
+{
+    public static partial class Resolver
+    {
+        public static RStatement Resolve(Statement statement)
+            => statement switch
+            {
+                ExpressionStatement expressionStatement => Resolve(expressionStatement),
+                ImportStatement importStatement => Resolve(importStatement),
+                _ => NotImplemented<RStatement>(statement)
+            };
+
+        public static RExpressionStatement Resolve(ExpressionStatement expressionStatement)
+            => new(Resolve(expressionStatement.Expression));
+    }
+}
