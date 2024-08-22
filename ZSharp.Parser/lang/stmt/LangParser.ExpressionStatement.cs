b6b2527145277531324e506e0c20c0aa2976e465
Binyamin Y Cohen <binyamincohen555@gmail.com>
2024-08-22 13:28:22 +0300

    Add expression statement parser

---
+++ ZSharp.Parser/lang/stmt/LangParser.ExpressionStatement.cs
@@ -0,0 +1,20 @@
+﻿using ZSharp.AST;
+
+namespace ZSharp.Parser
+{
+    public static partial class LangParser
+    {
+        public static ExpressionStatement ParseExpressionStatement(Parser parser)
+        {
+            var expression = parser.Parse<Expression>();
+
+            return new(new()
+                {
+                    Semicolon = parser.Eat(Text.TokenType.Semicolon)
+                })
+            {
+                Expression = expression
+            };
+        }
+    }
+}
