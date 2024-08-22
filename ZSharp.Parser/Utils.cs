8a36bfebd878f384b630211d286c616aa6e2f935
Binyamin Y Cohen <binyamincohen555@gmail.com>
2024-08-22 13:28:59 +0300

    Add utility functions

---
+++ ZSharp.Parser/Utils.cs
@@ -0,0 +1,38 @@
+﻿using ZSharp.AST;
+using ZSharp.Text;
+
+namespace ZSharp.Parser
+{
+    public delegate Expression TerminalFunction(Token token);
+
+    public static class Utils
+    {
+        //public static ParserFunction<Statement> ExpressionStatement(Func<Parser, Expression> fn)
+        //    => parser =>
+        //    {
+        //        var expression = fn(parser);
+        //        parser.Eat(TokenType.Semicolon);
+        //        return new ExpressionStatement(expression);
+        //    };
+
+        public static LedFunction InfixL(string @operator, int bindingPower)
+            => (parser, left) =>
+            {
+                parser.Eat(@operator);
+
+                var right =
+                (parser.GetParserFor<Expression>() as ExpressionParser ?? throw new ParseError())
+                .ParseExpression(parser, bindingPower);
+
+                return new BinaryExpression()
+                {
+                    Left = left,
+                    Right = right,
+                    Operator = @operator
+                };
+            };
+
+        public static LedFunction InfixR(string @operator, int bindingPower)
+            => InfixL(@operator, bindingPower - 1);
+    }
+}
