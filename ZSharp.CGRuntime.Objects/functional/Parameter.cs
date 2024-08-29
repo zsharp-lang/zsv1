eda4253414ab8586216f091b405de5d28f7d71af
Binyamin Y Cohen <binyamincohen555@gmail.com>
2024-08-29 21:48:23 +0300

    Add CG signature objects

---
+++ ZSharp.CGRuntime.Objects/functional/Parameter.cs
@@ -0,0 +1,13 @@
+﻿namespace ZSharp.CGObjects
+{
+    public sealed class Parameter(string name) : CGObject
+    {
+        public IR.Parameter? IR { get; set; }
+
+        public string Name { get; } = name;
+
+        public CGCode? Type { get; init; }
+
+        public CGCode? Initializer { get; init;}
+    }
+}
---
+++ ZSharp.CGRuntime.Objects/functional/Signature.cs
@@ -0,0 +1,15 @@
+﻿using Parameters = CommonZ.Utils.Collection<ZSharp.CGObjects.Parameter>;
+
+namespace ZSharp.CGObjects
+{
+    public sealed class Signature : CGObject
+    {
+        public Parameters Args { get; init; } = [];
+
+        public Parameter? VarArgs { get; set; }
+
+        public Parameters KwArgs { get; init; } = [];
+
+        public Parameter? VarKwArgs { get; set; }
+    }
+}
