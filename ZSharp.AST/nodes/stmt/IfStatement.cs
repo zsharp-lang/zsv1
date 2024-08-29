e19f370d8706e020974a3b04d1c45186751dbcc3
Binyamin Y Cohen <binyamincohen555@gmail.com>
2024-08-29 21:45:16 +0300

    Add if statement node

---
+++ ZSharp.AST/nodes/stmt/IfStatement.cs
@@ -0,0 +1,11 @@
+﻿namespace ZSharp.AST
+{
+    public sealed class IfStatement : Statement
+    {
+        public required Expression Condition { get; set; }
+
+        public required Statement If { get; set; }
+
+        public Statement? Else { get; set; }
+    }
+}
