5aead93fe9280691cd49380c2309dad9bab1848c
Binyamin Y Cohen <binyamincohen555@gmail.com>
2024-08-29 21:44:37 +0300

    Add case statement node

---
+++ ZSharp.AST/nodes/stmt/case/WhenClauseStatement.cs
@@ -0,0 +1,9 @@
+﻿namespace ZSharp.AST
+{
+    public sealed class WhenClauseStatement : Statement
+    {
+        public required Expression Value { get; set; }
+
+        public Statement? Body { get; set; }
+    }
+}
