3d7b35ddeaac3bbe242bf3d36a250d717bdd61b3
Binyamin Y Cohen <binyamincohen555@gmail.com>
2024-08-29 19:58:38 +0300

    Implement function AST node

---
+++ ZSharp.AST/nodes/def/function/Function.cs
@@ -0,0 +1,24 @@
+﻿namespace ZSharp.AST
+{
+    public class Function(FunctionTokens tokens) : Expression(tokens)
+    {
+        public new FunctionTokens TokenInfo
+        {
+            get => As<FunctionTokens>();
+            init => base.TokenInfo = value;
+        }
+
+        public string? Name { get; init; }
+
+        public required Signature Signature { get; set; }
+
+        public Expression? ReturnType { get; set; }
+
+        public Statement? Body { get; set; }
+
+        public override string ToString()
+            => $"fun{(Name is null ? string.Empty : $" {Name}")}" +
+            $"({Signature}){(ReturnType is null ? string.Empty : $": {ReturnType}")}" +
+            $"{(Body is null ? ";" : " {}")}";
+    }
+}
---
+++ ZSharp.AST/nodes/def/function/FunctionTokens.cs
@@ -0,0 +1,9 @@
+﻿using ZSharp.Text;
+
+namespace ZSharp.AST
+{
+    public class FunctionTokens : TokenInfo
+    {
+        public required Token FunKeyword { get; init; }
+    }
+}
