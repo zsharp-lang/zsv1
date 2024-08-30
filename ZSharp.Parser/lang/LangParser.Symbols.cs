75f61bd16aac651ac39dafa2dd43245bb6f74a41
Binyamin Y Cohen <binyamincohen555@gmail.com>
2024-08-30 17:47:35 +0300

    Add support for parsing member access operator

--- ZSharp.Parser/lang/LangParser.Symbols.cs
+++ ZSharp.Parser/lang/LangParser.Symbols.cs
@@ -6,6 +6,7 @@
         {
             public const string Assign          = "=";
             public const string ImportAll       = "*";
+            public const string MemberAccess    = ".";
             public const string ThenDo          = "=>";
             public const string VarPack         = "...";
         }
