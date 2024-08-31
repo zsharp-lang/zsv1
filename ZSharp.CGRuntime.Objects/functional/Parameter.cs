7dcfbb95b129d501375b93fdda4150538262532d
Binyamin Y Cohen <binyamincohen555@gmail.com>
2024-09-01 02:20:50 +0300

    Implement parameter CT readable protocol

--- ZSharp.CGRuntime.Objects/functional/Parameter.cs
+++ ZSharp.CGRuntime.Objects/functional/Parameter.cs
@@ -1,6 +1,11 @@
-﻿namespace ZSharp.CGObjects
+﻿using ZSharp.Compiler;
+using ZSharp.IR;
+
+namespace ZSharp.CGObjects
 {
-    public sealed class Parameter(string name) : CGObject
+    public sealed class Parameter(string name) 
+        : CGObject
+        , ICTReadable
     {
         public IR.Parameter? IR { get; set; }
 
@@ -9,5 +14,15 @@
         public CGCode? Type { get; init; }
 
         public CGCode? Initializer { get; init;}
+
+        IType ICTReadable.Type => IR!.Type;
+
+        public Code Read(ICompiler compiler)
+            => new([
+                new IR.VM.GetArgument(IR!),
+            ])
+            {
+                MaxStackSize = 1,
+            };
     }
 }
