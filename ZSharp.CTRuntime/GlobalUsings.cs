4c1927e46022659751bbc32fc49291dde1c71210
Binyamin Y Cohen <binyamincohen555@gmail.com>
2024-09-01 14:50:21 +0300

    Remove old CTRuntime project

--- ZSharp.CTRuntime/GlobalUsings.cs
+++
@@ -1,9 +0,0 @@
-﻿global using ZSValue = ZSharp.VM.ZSObject;
-global using ZSType = ZSharp.VM.ZSObject;
-
-global using Scope = CommonZ.Utils.Cache<ZSharp.RAST.RNode, ZSharp.VM.ZSObject>;
-
-global using CTObject = ZSharp.VM.ZSObject;
-
-global using MemberIndex = int;
-global using MemberName = string;
--- ZSharp.CTRuntime/concepts/callable/ICallable.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace ZSharp.CTRuntime
-{
-    public interface ICallable<T>
-    {
-        public T Call(ZSCompiler compiler, T callable, Argument<T>[] arguments);
-    }
-}
