aa79f53f1a4f874cba78ce7d4aa8175167c65328
Binyamin Y Cohen <biny@xpo.dev>
2024-09-02 16:58:08 +0300

    Implement ICTReadable for RawType

--- ZSharp.CGRuntime.Objects/raw/RawType.cs
+++ ZSharp.CGRuntime.Objects/raw/RawType.cs
@@ -1,14 +1,27 @@
 ﻿using ZSharp.Compiler;
+using ZSharp.IR;
 
 namespace ZSharp.CGObjects
 {
-    public sealed class RawType(IR.IType type)
+    public sealed class RawType(IType type)
         : CGObject
+        , ICTReadable
         //, ICTType
     {
-        private readonly IR.IType type = type;
+        private readonly IType type = type;
 
-        public IR.IType AsType(ICompiler compiler)
+        public IType Type => throw new NotImplementedException();
+
+        public IType AsType(ICompiler compiler)
             => type;
+
+        public Code Read(ICompiler compiler)
+            => type is IRObject ir ? new([
+                new IR.VM.GetObject(ir)
+            ])
+            {
+                MaxStackSize = 1,
+                Types = [type]
+            } : throw new();
     }
 }
