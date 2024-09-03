d27a37ac17c01ff905496dbd2dd15735cd757f6f
Binyamin Y Cohen <biny@xpo.dev>
2024-09-03 23:16:37 +0300

    Add CG local object

---
+++ ZSharp.CGRuntime.Objects/functional/Local.cs
@@ -0,0 +1,27 @@
+﻿using ZSharp.Compiler;
+using ZSharp.IR;
+
+namespace ZSharp.CGObjects
+{
+    public sealed class Local 
+        : CGObject
+        , ICTReadable
+    {
+        public IR.VM.Local? IR { get; set; }
+
+        public required string Name { get; set; }
+
+        public CGCode? Type { get; set; }
+
+        public CGCode? Initializer { get; set; }
+
+        IType ICTReadable.Type => IR!.Type;
+
+        public Code Read(ICompiler compiler)
+            => new([new IR.VM.GetLocal(IR!)])
+            {
+                MaxStackSize = 1,
+                Types = [IR!.Type]
+            };
+    }
+}
