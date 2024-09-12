fb966941551a47cffdc8c043c5cb8b237dfe54fb
Binyamin Y Cohen <biny@xpo.dev>
2024-09-13 00:27:15 +0300

    Add very basic support for literals in the compiler

--- ZSharp.Compiler.Core/interfaces/ICompiler.cs
+++ ZSharp.Compiler.Core/interfaces/ICompiler.cs
@@ -1,7 +1,19 @@
-﻿namespace ZSharp.Compiler
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace ZSharp.Compiler
 {
     public interface ICompiler
     {
         public Code CompileIRCode(CGObject @object);
+
+        public Code Assign(Code irCode, Assignment assignment)
+            => throw new NotImplementedException();
+
+        public Assignment AssignTo(CGObject source, CGObject target)
+            => throw new NotImplementedException();
+
+        public bool IsLiteral<T>(CGObject @object, [NotNullWhen(true)] out T? value) where T : class;
+
+        public bool IsCTValue<T>(CGObject @object, [NotNullWhen(true)] out T? value) where T : class;
     }
 }
---
+++ ZSharp.Compiler/compiler core/compiler/value/Compiler.IsCT.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace ZSharp.Compiler
+{
+    public sealed partial class Compiler
+        : ICompiler
+    {
+        public bool IsCTValue(CGObject @object)
+        {
+            if (IsLiteral(@object)) return true;
+
+            return false; // TODO: implement the CTValue protocol
+        }
+
+        public bool IsCTValue<T>(CGObject @object, [NotNullWhen(true)] out T? value)
+            where T : struct
+        {
+            if (IsLiteral(@object, out value)) return true;
+
+            return false; // TODO: implement CTValue protocol
+        }
+
+        public bool IsCTValue<T>(CGObject @object, [NotNullWhen(true)] out T? value)
+            where T : class
+        {
+            if (IsLiteral(@object, out value)) return true;
+
+            return false; // TODO: implement CTValue protocol
+        }
+    }
+}
