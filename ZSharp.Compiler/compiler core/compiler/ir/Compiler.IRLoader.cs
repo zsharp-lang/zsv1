bf226941146049d125ed422462f62b8a936684ed
Binyamin Y Cohen <biny@xpo.dev>
2024-10-29 17:03:39 +0200

    Add IR import system API

---
+++ ZSharp.Compiler/compiler core/compiler/ir/Compiler.IRLoader.cs
@@ -0,0 +1,31 @@
+﻿using CommonZ.Utils;
+using ZSharp.CGObjects;
+
+namespace ZSharp.Compiler
+{
+    public sealed partial class Compiler
+    {
+        private readonly ObjectCache objectCache = new();
+
+        public CGObject ImportIR(IR.IRObject ir)
+            => objectCache.CG(ir, out var result)
+            ? result
+            : objectCache.CG(ir, ir switch
+            {
+                IR.Module module => ImportIR(module),
+                _ => throw new NotImplementedException(),
+            });
+
+        private Module ImportIR(IR.Module module)
+        {
+            Module result = new(module.Name!)
+            {
+                IR = module,
+            };
+
+            // TODO: populate module members with CG representations.
+
+            return result;
+        }
+    }
+}
