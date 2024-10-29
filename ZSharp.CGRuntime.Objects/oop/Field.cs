28744d8354649bc566b67178d07294b16b012be5
Binyamin Y Cohen <biny@xpo.dev>
2024-10-29 16:57:37 +0200

    Add field and method CG objects

---
+++ ZSharp.CGRuntime.Objects/oop/Field.cs
@@ -0,0 +1,26 @@
+﻿using CommonZ.Utils;
+using ZSharp.Compiler;
+using ZSharp.IR;
+
+namespace ZSharp.CGObjects
+{
+    public sealed class Field(string name)
+        : CGObject
+        , ICTReadable
+    {
+        public IR.Field? IR { get; set; }
+
+        public string Name { get; set; } = name;
+
+        public CGObject? Initializer { get; set; }
+
+        public CGObject? Type { get; set; }
+
+        IType ICTReadable.Type => IR!.Type;
+
+        public Code Read(ICompiler compiler)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ZSharp.CGRuntime.Objects/oop/Method.cs
@@ -0,0 +1,12 @@
+﻿using ZSharp.Compiler;
+
+namespace ZSharp.CGObjects
+{
+    public sealed class Method(string? name)
+        : CGObject
+    {
+        public IR.Method? IR { get; set; }
+
+        public string? Name { get; set; } = name;
+    }
+}
