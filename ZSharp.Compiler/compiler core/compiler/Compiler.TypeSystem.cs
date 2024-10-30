cc1ef155e4e794ed37d0e8b64b5814b0da545b1a
Binyamin Y Cohen <biny@xpo.dev>
2024-10-30 11:23:00 +0200

    Add i32 and f32 to compiler type system

--- ZSharp.Compiler/compiler core/compiler/Compiler.TypeSystem.cs
+++ ZSharp.Compiler/compiler core/compiler/Compiler.TypeSystem.cs
@@ -9,6 +9,10 @@
             Context.GlobalScope.Cache("string", TypeSystem.String);
             Context.GlobalScope.Cache("type", TypeSystem.Type);
             Context.GlobalScope.Cache("void", TypeSystem.Void);
+
+            Context.GlobalScope.Cache("i32", TypeSystem.Int32);
+
+            Context.GlobalScope.Cache("f32", TypeSystem.Float32);
         }
     }
 }
