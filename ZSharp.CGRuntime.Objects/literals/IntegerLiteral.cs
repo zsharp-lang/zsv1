9601261713e46cb1fb19f84594945a9e05c1ee58
Binyamin Y Cohen <biny@xpo.dev>
2024-10-30 11:22:14 +0200

    Add integer and f32 CG objects

---
+++ ZSharp.CGRuntime.Objects/literals/IntegerLiteral.cs
@@ -0,0 +1,23 @@
+﻿using ZSharp.Compiler;
+using ZSharp.IR;
+
+namespace ZSharp.CGObjects
+{
+    public sealed class IntegerLiteral(DefaultIntegerType value, IType type)
+        : Literal<DefaultIntegerType>(value)
+        , ICTReadable
+    {
+        public override IType Type { get; } = type;
+
+        public override Code Read(ICompiler compiler)
+            => new(
+                [
+                    new IR.VM.PutInt32(Value)
+                ]
+            )
+            {
+                MaxStackSize = 1,
+                Types = [Type]
+            };
+    }
+}
