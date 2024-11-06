6a9d33df60c6789fc6ff3cc87dac8ec98232440e
Binyamin Y Cohen <biny@xpo.dev>
2024-11-06 13:36:02 +0200

    Add method body compiler

---
+++ ZSharp.Compiler/compilers/module/content/oop/class/content/method/MethodBodyCompiler.Compile.cs
@@ -0,0 +1,32 @@
+﻿using ZSharp.CGObjects;
+using ZSharp.RAST;
+
+namespace ZSharp.Compiler
+{
+    internal sealed partial class MethodBodyCompiler
+        : IStatementCompiler
+    {
+        CGObject? IStatementCompiler.Compile(RStatement statement)
+            => statement switch
+            {
+                RReturn @return => Compile(@return),
+                _ => null
+            };
+
+        private CGObject Compile(RReturn @return)
+        {
+            CGObject? value = 
+                @return.Value is null ? null : Compiler.CompileNode(@return.Value);
+
+            var code = value is null ? null : Compiler.CompileIRCode(value);
+
+            return new RawCode(new([
+                ..code?.Instructions ?? [],
+                new IR.VM.Return(),
+                ])
+            {
+                MaxStackSize = code?.MaxStackSize ?? 0,
+            });
+        }
+    }
+}
