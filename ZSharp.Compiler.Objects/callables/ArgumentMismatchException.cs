ad1de1689e4373e1703778c0dcbca7654e72b331
Binyamin Y Cohen <biny@xpo.dev>
2025-01-02 14:59:30 +0200

    Add ArgumentMismatchException

---
+++ ZSharp.Compiler.Objects/callables/ArgumentMismatchException.cs
@@ -0,0 +1,12 @@
+﻿using ZSharp.Compiler;
+
+namespace ZSharp.Objects
+{
+    public class ArgumentMismatchException(CompilerObject callable, Argument[] arguments)
+        : CompilerObjectException(callable)
+    {
+        public CompilerObject Callable => Object;
+
+        public Argument[] Arguments { get; } = arguments;
+    }
+}
