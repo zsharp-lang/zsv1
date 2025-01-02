eeadaff18d2606a0629b3221fd42497619a6bcb2
Binyamin Y Cohen <biny@xpo.dev>
2025-01-02 14:54:12 +0200

    Add base compiler exception

---
+++ ZSharp.Compiler/exceptions/CompilerObjectException.cs
@@ -0,0 +1,8 @@
+﻿namespace ZSharp.Compiler
+{
+    public abstract class CompilerObjectException(CompilerObject @object, string? message = null)
+        : Exception(message)
+    {
+        public CompilerObject Object { get; } = @object;
+    }
+}
