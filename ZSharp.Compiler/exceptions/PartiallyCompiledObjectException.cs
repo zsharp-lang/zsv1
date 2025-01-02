b90635eca76560776308a7ba6642df30247f4b1f
Binyamin Y Cohen <biny@xpo.dev>
2025-01-02 14:58:21 +0200

    Add PartiallyCompiledObjectException

---
+++ ZSharp.Compiler/exceptions/PartiallyCompiledObjectException.cs
@@ -0,0 +1,11 @@
+﻿namespace ZSharp.Compiler
+{
+    public sealed class PartiallyCompiledObjectException(
+        CompilerObject @object, 
+        string? message = null
+    )
+        : CompilerObjectException(@object, message)
+    {
+
+    }
+}
