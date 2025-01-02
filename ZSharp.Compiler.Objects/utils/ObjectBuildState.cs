29669986c242991d4e6ebcdc7606ae0d929c903b
Binyamin Y Cohen <biny@xpo.dev>
2025-01-02 15:01:44 +0200

    Add object build state helper class

---
+++ ZSharp.Compiler.Objects/utils/ObjectBuildState.cs
@@ -0,0 +1,15 @@
+﻿namespace ZSharp.Objects
+{
+    internal sealed class ObjectBuildState<T>
+        where T : struct, Enum, IConvertible
+    {
+        public T State { get; private set; }
+
+        public bool Get(T state) => State.HasFlag(state);
+
+        public void Set(T state)
+        {
+            State = (T)(ValueType)(State.ToInt32(null) | state.ToInt32(null));
+        }
+    }
+}
