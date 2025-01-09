35a78b89338a247fadbdfd80693ddc367b63c6db
Binyamin Y Cohen <biny@xpo.dev>
2025-01-09 14:57:30 +0200

    Add tokens to oop definition node

--- ZSharp.AST/nodes/def/oop/OOPDefinition.cs
+++ ZSharp.AST/nodes/def/oop/OOPDefinition.cs
@@ -1,7 +1,13 @@
 ﻿namespace ZSharp.AST
 {
-    public class OOPDefinition : Expression
+    public class OOPDefinition(OOPDefinitionTokens tokens) : Expression(tokens)
     {
+        public new OOPDefinitionTokens TokenInfo
+        {
+            get => As<OOPDefinitionTokens>();
+            init => base.TokenInfo = value;
+        }
+
         public required string Type { get; set; }
 
         public string Name { get; set; } = string.Empty;
