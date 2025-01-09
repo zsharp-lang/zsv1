30fac0cd68bb8e1ee1c2ea3361b10a89e92222d7
Binyamin Y Cohen <biny@xpo.dev>
2025-01-09 14:56:41 +0200

    Add tokens to module node

---
+++ ZSharp.AST/nodes/def/module/ModuleTokens.cs
@@ -0,0 +1,11 @@
+﻿using ZSharp.Text;
+
+namespace ZSharp.AST
+{
+    public sealed class ModuleTokens : TokenInfo
+    {
+        public Token ModuleKeyword { get; init; }
+
+        public Token Name { get; init; }
+    }
+}
