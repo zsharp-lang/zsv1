bf3d0dcb300c186c1e0d5a2353805fcbd8f32981
Binyamin Y Cohen <biny@xpo.dev>
2025-02-01 23:24:18 +0200

    Remove problematic code

--- ZSharp.Compiler/cg objects/Type.cs
+++ ZSharp.Compiler/cg objects/Type.cs
@@ -5,24 +5,24 @@ namespace ZSharp.Objects
 {
     internal sealed class Type(IRType type)
         : CompilerObject
-        , ICTReadable
+        //, ICTReadable
         , ICompileIRType
     {
         private readonly IRType type = type;
-        private readonly IRObject ir = type as IRObject ?? throw new();
+        //private readonly IRObject ir = type as IRObject ?? throw new();
 
-        CompilerObject ITyped.Type => this;
+        //CompilerObject ITyped.Type => this;
 
         IType ICompileIRType.CompileIRType(Compiler.Compiler compiler)
             => type;
 
-        public IRCode Read(Compiler.Compiler compiler)
-        => new([
-                new IR.VM.GetObject(ir)
-            ])
-            {
-                MaxStackSize = 1,
-                Types = [this]
-            };
+        //public IRCode Read(Compiler.Compiler compiler)
+        //=> new([
+        //        new IR.VM.GetObject(ir)
+        //    ])
+        //    {
+        //        MaxStackSize = 1,
+        //        Types = [this]
+        //    };
     }
 }
