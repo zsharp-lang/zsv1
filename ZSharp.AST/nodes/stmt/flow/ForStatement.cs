5781e1aa7ec96e6027f1377b39eda416305fc0ba
Binyamin Y Cohen <biny@xpo.dev>
2025-02-02 22:34:50 +0200

    Add `for` statement AST node

---
+++ ZSharp.AST/nodes/stmt/flow/ForStatement.cs
@@ -0,0 +1,13 @@
+﻿namespace ZSharp.AST
+{
+    public sealed class ForStatement : Statement
+    {
+        public required Expression CurrentItem { get; set; }
+
+        public required Expression Iterable { get; set; }
+
+        public required Statement Body { get; set; }
+
+        public Statement? Else { get; set; }
+    }
+}
