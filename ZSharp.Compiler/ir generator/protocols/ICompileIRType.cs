45493b973228a9e12be9e82fbe88d66770b28d1f
Binyamin Y Cohen <biny@xpo.dev>
2025-02-28 00:46:35 +0200

    Add generic CompileIRType protocol

--- ZSharp.Compiler/ir generator/protocols/ICompileIRType.cs
+++ ZSharp.Compiler/ir generator/protocols/ICompileIRType.cs
@@ -4,4 +4,13 @@
     {
         public IRType CompileIRType(Compiler compiler);
     }
+
+    public interface ICompileIRType<T> : ICompileIRType
+        where T : IRType
+    {
+        IRType ICompileIRType.CompileIRType(Compiler compiler)
+            => CompileIRType(compiler);
+
+        public new T CompileIRType(Compiler compiler);
+    }
 }
