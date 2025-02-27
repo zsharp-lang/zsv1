8d95eb06d34ac0b6f1d9b7b781b440fd5d56b12d
Binyamin Y Cohen <biny@xpo.dev>
2025-02-28 00:35:19 +0200

    Make IR constructor an IR object

--- ZSharp.IR/ir/oop/Constructor.cs
+++ ZSharp.IR/ir/oop/Constructor.cs
@@ -1,7 +1,10 @@
 ﻿namespace ZSharp.IR
 {
     public sealed class Constructor(string? name)
+        : IRObject
     {
+        public override Module? Module => Method.Module;
+
         public string? Name { get; set; } = name;
 
         public Method Method { get; set; }
