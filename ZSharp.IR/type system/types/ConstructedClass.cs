9dc37ca58ec2dfdcbc5bbac4476b413a4e35cd6f
Binyamin Y Cohen <biny@xpo.dev>
2025-04-06 19:54:44 +0300

    Add base type reference interface

--- ZSharp.IR/type system/types/ConstructedClass.cs
+++ ZSharp.IR/type system/types/ConstructedClass.cs
@@ -3,9 +3,13 @@
 namespace ZSharp.IR
 {
     public sealed class ConstructedClass(Class @class)
-        : IType
+        : ConstructedType<Class>
     {
         public Class Class { get; set; } = @class;
+
+        Class OOPTypeReference<Class>.Definition => Class;
+
+        public OOPTypeReference? OwningType { get; set; }
 
         public Collection<IType> Arguments { get; set; } = @class.HasGenericParameters ? [] : Collection<IType>.Empty;
     }
---
+++ ZSharp.IR/type system/types/ConstructedType.cs
@@ -0,0 +1,18 @@
+﻿using CommonZ.Utils;
+
+namespace ZSharp.IR
+{
+    public interface ConstructedType
+        : OOPTypeReference
+    {
+        public abstract Collection<IType> Arguments { get; }
+    }
+
+    public interface ConstructedType<T> 
+        : ConstructedType
+        , OOPTypeReference<T>
+        where T : OOPType
+    {
+        
+    }
+}
