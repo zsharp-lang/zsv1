71c55f51fe57c68f1015bebd69b6ad8d3badb085
Binyamin Y Cohen <biny@xpo.dev>
2025-04-06 19:55:02 +0300

    Add base member reference interface

---
+++ ZSharp.IR/ir/oop/MemberReference.cs
@@ -0,0 +1,14 @@
+﻿namespace ZSharp.IR
+{
+    public interface MemberReference
+    {
+        public OOPTypeReference OwningType { get; }
+    }
+
+    public interface MemberReference<T>
+        : MemberReference
+        where T : IRObject
+    {
+        public T Member { get; }
+    }
+}
