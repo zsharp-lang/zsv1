4aea0f72f99e4cc0e99a0f46c00db49fda9a9052
Binyamin Y Cohen <biny@xpo.dev>
2025-04-06 19:55:45 +0300

    Add field reference class

---
+++ ZSharp.IR/ir/oop/field/FieldReference.cs
@@ -0,0 +1,10 @@
+﻿namespace ZSharp.IR
+{
+    public sealed class FieldReference(Field field)
+        : MemberReference<Field>
+    {
+        public Field Member { get; set; } = field;
+
+        public required OOPTypeReference OwningType { get; set; }
+    }
+}
