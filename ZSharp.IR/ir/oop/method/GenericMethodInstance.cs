d56fc5f86712a88ab6cb6ce09b74ebf89647a39a
Binyamin Y Cohen <biny@xpo.dev>
2025-04-06 19:55:34 +0300

    Add generic method instance class

---
+++ ZSharp.IR/ir/oop/method/GenericMethodInstance.cs
@@ -0,0 +1,23 @@
+﻿using CommonZ.Utils;
+
+namespace ZSharp.IR
+{
+    public sealed class GenericMethodInstance(MethodReference method)
+        : MemberReference<Method>
+        , ICallable
+    {
+        public MethodReference Method { get; set; } = method;
+
+        public Collection<IType> Arguments { get; } = /*method.Member.HasGenericParameters*/false ? [] : Collection<IType>.Empty;
+
+        public Method Member => Method.Member;
+
+        public OOPTypeReference OwningType => Method.OwningType;
+
+        public Signature Signature => Method.Signature;
+
+        public bool HasBody => Method.HasBody;
+
+        public ICallableBody? Body => Method.HasBody ? Method.Body : null;
+    }
+}
