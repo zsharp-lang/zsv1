be5c2c4dbc89972a9a4a1c38157efee026c91e92
Binyamin Y Cohen <biny@xpo.dev>
2025-04-06 19:55:21 +0300

    Add method reference class

---
+++ ZSharp.IR/ir/oop/method/MethodReference.cs
@@ -0,0 +1,17 @@
+﻿namespace ZSharp.IR
+{
+    public sealed class MethodReference(Method method)
+        : MemberReference<Method>
+        , ICallable
+    {
+        public Method Member { get; set; } = method;
+
+        public required OOPTypeReference OwningType { get; set; }
+
+        public Signature Signature => Member.Signature;
+
+        public bool HasBody => Member.HasBody;
+
+        public ICallableBody? Body => Member.HasBody ? Member.Body : null;
+    }
+}
