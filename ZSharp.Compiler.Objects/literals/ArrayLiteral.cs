3e37974c3008869e3f2428f7853d3a1c7196beb2
Binyamin Y Cohen <biny@xpo.dev>
2025-04-23 00:10:51 +0300

    Add support for array literal initialization

--- ZSharp.Compiler.Objects/literals/ArrayLiteral.cs
+++ ZSharp.Compiler.Objects/literals/ArrayLiteral.cs
@@ -1,8 +1,41 @@
-﻿namespace ZSharp.Objects
+﻿using ZSharp.Compiler;
+
+namespace ZSharp.Objects
 {
     public sealed class ArrayLiteral(IEnumerable<CompilerObject>? items = null)
         : CompilerObject
+        , ICTTypeCast
     {
         public List<CompilerObject> Items { get; } = new(items ?? []);
+
+        CompilerObject ICTTypeCast.Cast(Compiler.Compiler compiler, CompilerObject targetType)
+        {
+            var instance = compiler.Call(targetType, []);
+
+            var code = new IRCode(compiler.CompileIRCode(instance).Instructions);
+
+            var append = compiler.Member(targetType, "append");
+
+            foreach (var item in Items)
+                code.Instructions.AddRange(
+                    compiler.CompileIRCode(
+                        compiler.Call(append, [
+                            new(new RawCode(new([
+                                    new IR.VM.Dup()
+                                ])
+                                {
+                                    Types = [targetType]
+                                })
+                            ),
+                            new(item)
+                        ])
+                    ).Instructions
+                );
+
+            code.Types.Clear();
+            code.Types.Add(targetType);
+
+            return new RawCode(code);
+        }
     }
 }
