f5ad8a228cb970d1a41bf73a1e550c929adaba89
Binyamin Y Cohen <biny@xpo.dev>
2025-04-23 23:28:46 +0300

    Add compiler Result type

---
+++ ZSharp.Compiler/compiler core/compiler/Compiler.DRY.cs
@@ -0,0 +1,16 @@
+﻿namespace ZSharp.Compiler
+{
+    public sealed partial class Compiler
+    {
+        public CompilerObjectResult Wrap(Func<Compiler, CompilerObject> func)
+        {
+            try
+            {
+                return CompilerObjectResult.Ok(func(this));
+            } catch (CompilerObjectException e)
+            {
+                return CompilerObjectResult.Error(e.Message);
+            }
+        }
+    }
+}
---
+++ ZSharp.Compiler/core/Result.cs
@@ -0,0 +1,55 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace ZSharp.Compiler
+{
+    public sealed class Result<TResult, TError>
+        where TResult : class
+        where TError : class
+    {
+        private readonly TResult? result;
+        private readonly TError? error;
+
+        [MemberNotNullWhen(true, nameof(result))]
+        public bool IsOk => result is not null;
+
+        [MemberNotNullWhen(true, nameof(error))]
+        public bool IsError => error is not null;
+
+        private Result(TResult? result, TError? error)
+        {
+            this.result = result;
+            this.error = error;
+        }
+
+        public static Result<TResult, TError> Ok(TResult result)
+            => new(result, null);
+
+        public static Result<TResult, TError> Error(TError error)
+            => new(null, error);
+
+        public bool Ok([NotNullWhen(true)] out TResult? result)
+            => (result = this.result) is not null;
+
+        public bool Error([NotNullWhen(true)] out TError? error)
+            => (error = this.error) is not null;
+
+        public TResult Unwrap()
+            => result ?? throw new InvalidOperationException();
+
+        public Result<TResult, TError> When(Action<TResult> action)
+        {
+            if (IsOk)
+                action(result);
+
+            return this;
+        }
+
+        public Result<TResult, TError> Else(Action<TError> action)
+        {
+            if (IsError)
+                action(error);
+
+            return this;
+        }
+    }
+}
