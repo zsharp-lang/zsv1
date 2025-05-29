863c4d0621abac5a484c46a92d42485e09cf6d17
Binyamin Y Cohen <biny@xpo.dev>
2025-05-29 16:13:17 +0300

    Fix IsOfExpression node

--- ZSharp.AST/nodes/expr/cast/IsOfExpression.cs
+++ ZSharp.AST/nodes/expr/cast/IsOfExpression.cs
@@ -2,7 +2,7 @@
 {
     public sealed class IsOfExpression : Expression
     {
-        public Expression Expression { get; set; }
+        public required Expression Expression { get; set; }
 
         public string? Name { get; set; }
 
