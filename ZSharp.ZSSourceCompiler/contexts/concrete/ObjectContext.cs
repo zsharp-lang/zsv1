ee6d431e955f6fadf9232f379ef044fd027811f1
Binyamin Y Cohen <biny@xpo.dev>
2025-06-08 23:11:18 +0300

    Add missing file to source compiler

---
+++ ZSharp.ZSSourceCompiler/contexts/concrete/ObjectContext.cs
@@ -0,0 +1,13 @@
+﻿using ZSharp.Compiler;
+
+namespace ZSharp.ZSSourceCompiler
+{
+    public class ObjectContext<T>(T @object)
+        : IObjectContext<T>
+        where T : CompilerObject
+    {
+        public IContext? Parent { get; set; }
+
+        public T Object { get; } = @object;
+    }
+}
---
+++ ZSharp.ZSSourceCompiler/extensibility/error/errors/CompilationError.cs
@@ -0,0 +1,12 @@
+﻿namespace ZSharp.ZSSourceCompiler
+{
+    public sealed class CompilationError(
+        Node node,
+        string error
+    ) : Error
+    {
+        public Node Node { get; } = node;
+
+        public string Error { get; } = error;
+    }
+}
