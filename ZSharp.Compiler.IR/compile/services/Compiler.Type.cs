29dca0a62ca01278f6e600023f0b67aa6967c912
Binyamin Y Cohen <biny@xpo.dev>
2025-07-21 12:32:01 +0300

    Add CompileType methods

--- ZSharp.Compiler.IR/compile/services/Compiler.Type.cs
+++ ZSharp.Compiler.IR/compile/services/Compiler.Type.cs
@@ -2,6 +2,15 @@
 {
     partial class Compiler
     {
-        
+        public ZSharp.Compiler.Result<IR.IType, Error> CompileType(CompilerObject @object)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ZSharp.Compiler.Result<T, Error> CompileType<T>(CompilerObject @object)
+            where T : class, IR.IType
+        {
+            throw new NotImplementedException();
+        }
     }
 }
