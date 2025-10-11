7e46d6ecb95486305cb2c66e1677c63bceb5b150
Binyamin Y Cohen <biny@xpo.dev>
2025-10-11 23:36:41 +0300

    Fix delegate exposer

--- ZSharp.Importer.ILLoader/il loader/ILLoader.Expose.cs
+++ ZSharp.Importer.ILLoader/il loader/ILLoader.Expose.cs
@@ -46,11 +46,13 @@ namespace ZSharp.Importer.ILLoader
 
         public CompilerObject Expose(Delegate @delegate)
         {
-            var wrapped = new ExposedObjectWrapper(@delegate.Target, @delegate.Method);
+            //var wrapped = new ExposedObjectWrapper(@delegate.Target, @delegate.Method);
             // TODO: Cache this method info
             // TODO: Add support for array. Currently not supported because there's no make-array instruction in IR
-            var methodCO = LoadMethod(typeof(ExposedObjectWrapper).GetMethod("Invoke", [typeof(object)]) ?? throw new());
-            var @object = ExposeAsIR(wrapped);
+            //var methodCO = LoadMethod(typeof(ExposedObjectWrapper).GetMethod("Invoke", [typeof(object)]) ?? throw new());
+            //var @object = ExposeAsIR(wrapped);
+            var @object = ExposeAsIR(@delegate.Target);
+            var methodCO = LoadMethod(@delegate.Method);
             var objectCode = new RawIRCode(new(@object));
 
             return new Objects.BoundMethod()
