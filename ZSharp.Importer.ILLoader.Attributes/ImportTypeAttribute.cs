bf62dd370d031f957869a453ae54fa57d31de737
Binyamin Y Cohen <biny@xpo.dev>
2025-10-15 23:05:05 +0300

    Add support for forwarded types in IL modules exposed to Z#

---
+++ ZSharp.Importer.ILLoader.Attributes/ImportTypeAttribute.cs
@@ -0,0 +1,15 @@
+﻿namespace ZSharp.Importer.ILLoader
+{
+    [AttributeUsage(
+        AttributeTargets.Module | AttributeTargets.Class,
+        AllowMultiple = true
+    )]
+    public sealed class ImportTypeAttribute(Type type) : Attribute
+    {
+        public Type Type { get; } = type;
+
+        public string? Alias { get; init; } = null;
+
+        public string? Namespace { get; init; } = null;
+    }
+}
